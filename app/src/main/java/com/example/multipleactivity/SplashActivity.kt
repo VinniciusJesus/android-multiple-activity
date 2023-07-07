7ea13d0260effb4e317470ca0f5d41dc18117876
JMarcos VInicius <viniciusdejesus565@gmail.com>
2023-07-07 15:44:52 -0300

    update

---
+++ app/src/main/java/com/example/multipleactivity/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.multipleactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed( {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 3000)
+    }
+}
